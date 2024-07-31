db55730a94af54681cc05133337f8b43e1627fa9
stud3ntjan <152269508+stud3ntjan@users.noreply.github.com>
2024-07-31 13:54:11 +0200

    update

--- terraform/3_struktur_ec2/provider.tf
+++ terraform/3_struktur_ec2/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = ""
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock-table"
+  }
+}
